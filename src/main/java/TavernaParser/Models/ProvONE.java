d3937d951c43e68252616ea2aff28c66ffe1e7e0
Yan Mendes <ymendesf@gmail.com>
2018-08-14 16:07:51 -0300

    Changing the models values

--- src/main/java/TavernaParser/Models/ProvONE.java
+++ src/main/java/TavernaParser/Models/ProvONE.java
@@ -1,22 +1,22 @@
 package TavernaParser.Models;
 
 public class ProvONE {
-    public static final String WORKFLOW = "ProvONE_Workflow";
-    public static final String PROGRAM = "ProvONE_Program";
-    public static final String PORT = "ProvONE_Port";
-    public static final String CHANNEL = "ProvONE_Channel";
-    public static final String CONTROLLER = "ProvONE_Controller";
-    public static final String EXECUTION = "ProvONE_Execution";
-    public static final String USER = "ProvONE_User";
-    public static final String VISUALIZATION = "ProvONE_Visualization";
-    public static final String DATA = "ProvONE_Data";
-    public static final String DOCUMENT = "ProvONE_Document";
-    public static final String HADINPORT = "ProvONE_hadInPort";
-    public static final String HADOUTPORT = "ProvONE_hadOutPort";
-    public static final String HASINPORT = "ProvONE_hasInPort";
-    public static final String HASOUTPORT = "ProvONE_hasOutPort";
-    public static final String HADENTITY = "ProvONE_hadEntity";
-    public static final String HASSUBPROGRAM = "ProvONE_hasSubProgram";
-    public static final String HASDEFAULTPARAM = "ProvONE_hasDefaultParam";
-    public static final String WASPARTOF = "ProvONE_wasPartOf";
+    public static final String WORKFLOW = "provone_Workflow";
+    public static final String PROGRAM = "provone_Program";
+    public static final String PORT = "provone_Port";
+    public static final String CHANNEL = "provone_Channel";
+    public static final String CONTROLLER = "provone_Controller";
+    public static final String EXECUTION = "provone_Execution";
+    public static final String USER = "provone_User";
+    public static final String VISUALIZATION = "provone_Visualization";
+    public static final String DATA = "provone_Data";
+    public static final String DOCUMENT = "provone_Document";
+    public static final String HADINPORT = "provone_hadInPort";
+    public static final String HADOUTPORT = "provone_hadOutPort";
+    public static final String HASINPORT = "provone_hasInPort";
+    public static final String HASOUTPORT = "provone_hasOutPort";
+    public static final String HADENTITY = "provone_hadEntity";
+    public static final String HASSUBPROGRAM = "provone_hasSubProgram";
+    public static final String HASDEFAULTPARAM = "provone_hasDefaultParam";
+    public static final String WASPARTOF = "provone_wasPartOf";
 }
